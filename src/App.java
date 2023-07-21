1b64ce3f84a6b9eab891089d673ca278fb3012d3
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-07-21 17:30:07 +0300

    Decided Plan

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        QuizMaker quizMaker = new QuizMaker();
+        QuizQuestion quizQuestion = quizMaker.createQuizQuestion();
+
         QuizPage quizApp = new QuizPage();
+        quizApp.setQuestion(quizQuestion);
+
+        Answer answer = quizApp.getAnswer();
+
+        boolean truth = quizQuestion.checkAnswer(answer);
+        quizApp.setTruth(truth);
     }
 }
